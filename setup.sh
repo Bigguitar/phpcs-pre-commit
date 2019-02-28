7d8b735071076575b106b0ff5ec02f9c7b7466eb
Yuranan Wasikarn <yuranan.was@ascendcorp.com>
2019-02-28 16:18:53 +0700

    fix dir

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ else
     PRE_COMMIT_EXISTS=0
 fi
 
-cp ./hooks/pre-commit .git/hooks/pre-commit
+cp ./phpcs-pre-commit/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
 if [[ "$PRE_COMMIT_EXISTS" = 0 ]];
